bfc9a93e9f00cfdd05dbcd24bdcbeee4b45b1cc8
Matt Bergstedt <C16matthew.bergstedt@usafa.edu>
2014-01-27 11:23:43 -0700

    adjusted vhd file to use std_logic_vector for 8 bit

--- Lab1_Bergstedt.vhd
+++ Lab1_Bergstedt.vhd
@@ -19,6 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+--include numeric_std
+use IEEE.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,21 +32,29 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Lab1_Bergstedt is
-    Port ( A : in  STD_LOGIC;
-           B : in  STD_LOGIC;
-           C : in  STD_LOGIC;
-           X : out  STD_LOGIC;
-           Y : out  STD_LOGIC;
-           Z : out  STD_LOGIC);
+    Port ( --A : in  STD_LOGIC;
+           --B : in  STD_LOGIC;
+           --C : in  STD_LOGIC;
+			  -- create a std_logic_vector for input
+			  input : in STD_LOGIC_VECTOR(7 downto 0);
+			  
+           --X : out  STD_LOGIC;
+           --Y : out  STD_LOGIC;
+           --Z : out  STD_LOGIC;
+			  -- create a std_logic_vector for output
+			  output : out STD_LOGIC_VECTOR(7 downto 0));
 end Lab1_Bergstedt;
 
 architecture Behavioral of Lab1_Bergstedt is
 
-signal name1: std_logic;
-signal name2: std_logic_vector(3 downto 0);
+--signal name1: std_logic;
 
 begin
 
+--X <= (not A and B) or (not A and C) or (A and not B and not C);
+--Y <= (not C and B) or (not B and C);
+--Z <= C;
+output <= std_logic_vector(unsigned(not(input)) + 1);
 
 end Behavioral;
 
